62966b94ce6b80e1e9f7dcfc915a3b2f11f33673
dhruv-ojha-02 <dhruvojha02@gmail.com>
2024-06-04 20:50:34 +0530

    add hotel feature done

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,8 +2,11 @@ import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-d
 import Layout from "./layouts/Layout";
 import Register from "./pages/Register";
 import SignIn from "./pages/SignIn";
+import AddHotel from "./pages/AddHotel";
+import { useAppContext } from "./contexts/AppContext";
 
 const App = () => {
+  const {isLoggedIn}  = useAppContext();
   return (
     <Router>
       <Routes>
@@ -15,6 +18,11 @@ const App = () => {
         </Layout>} />
         <Route path="register" element={<Layout> < Register /> </Layout>} />
         <Route path="/sign-in" element={<Layout><SignIn /></Layout>} />
+        {isLoggedIn && (
+          <>
+            <Route path ="add-hotel" element=<Layout> <AddHotel /></Layout> />
+          </>
+        )}
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
