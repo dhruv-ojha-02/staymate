676c5d25bef2384a622e82ce539a8e614951131c
dhruv-ojha-02 <dhruvojha02@gmail.com>
2024-06-20 01:40:46 +0530

    final commit

--- frontend/src/components/SearchResultsCard.tsx
+++ frontend/src/components/SearchResultsCard.tsx
@@ -52,7 +52,7 @@ const SearchResultsCard = ({ hotel }: Props) => {
             <span className="font-bold">₹{hotel.pricePerNight} per night</span>
             <Link
               to={`/detail/${hotel._id}`}
-              className="bg-blue-600 text-white h-full p-2 font-bold text-xl max-w-fit hover:bg-blue-500"
+              className="bg-blue-600 text-white h-full p-2 font-bold text-xl max-w-fit hover:bg-blue-500 rounded-lg"
             >
               View More
             </Link>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#F5385D',
+        secondary: '#e31c5f',
+      },
+    },
     container: {
       padding: {
         md: "10rem",
